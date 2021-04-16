eea3238b90b4f95b4bdd87164862904ad23608fb
Manish Kumar <kumarmanish5848yadav@gmail.com>
2021-04-16 15:26:36 +0530

    noice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.52, date: new Date(2021, 5, 21) },
+    { title: "Car wash", amount: 24.52, date: new Date(2021, 5, 21) },
+    { title: "Car Insurance", amount: 294.52, date: new Date(2021, 5, 21) },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
